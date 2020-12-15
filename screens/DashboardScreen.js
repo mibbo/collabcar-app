61019d939399e800e09989484b606f8e3321c9f5
Tommi Mikkola <dobatus@gmail.com>
2020-12-15 15:25:57 +0200

    GoogleLogin: implement sign out and function for repeating sign in

--- screens/DashboardScreen.js
+++ screens/DashboardScreen.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Button } from 'react-native';
+import firebase from 'firebase';
 
 class DashboardScreen extends Component {
    render() {
       return (
-         <View style={StyleSheet.container}>
+         <View style={styles.container}>
             <Text>DashboardScreen</Text>
+            <Button title="Sign out" onPress={() => firebase.auth().signOut()} />
          </View>
       );
    }
